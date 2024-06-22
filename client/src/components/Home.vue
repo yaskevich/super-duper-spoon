198e2f116e23a9735e4cee22c473226a9757ad84
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-06-22 17:37:09 +0200

    implement autocomplete

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,17 +2,35 @@
   <div class="home">
     <div v-if="isLoaded" style="text-align: center; max-width: 350px; margin: auto">
       <n-card :bordered="false">
-        here will be dragons
-      </n-card>
+        <n-auto-complete v-model:value="query" :input-props="{
+      autocomplete: 'disabled'
+    }" :options="options" placeholder="..."  clearable  @update:value="getOptions" />
+    <!-- :on-update:value="getOptions"   -->
+    </n-card>
     </div>
     <div v-else style="text-align: center">...loading</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, onBeforeMount } from 'vue';
+import { ref, reactive, onBeforeMount, computed } from 'vue';
 import axios from 'axios';
 import store from '../store';
+
+store.state.token = 'test';
+
+const query = ref('');
+const options = ref([]);
+
+const getOptions = async () => {
+  const res = await store.get('suggestions', query.value);
+  // console.log(res);
+  options.value = res.map((item: any) => ({
+    label: `${item.title} (${item.lid})`,
+    value: item.id
+  }));
+};
+
 
 const isLoaded = ref(false);
 // const datum = reactive([]);
