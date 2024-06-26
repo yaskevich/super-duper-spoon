1520416e97e030f0a05eee7d189574339da78f13
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-06-26 20:44:44 +0200

    render entries

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,9 +4,11 @@
       <n-card :bordered="false">
         <n-auto-complete v-model:value="query" :input-props="{
       autocomplete: 'disabled'
-    }" :options="options" placeholder="..."  clearable  @update:value="getOptions" />
-    <!-- :on-update:value="getOptions"   -->
-    </n-card>
+    }" :options="options" placeholder="..." clearable @update:value="getOptions" :render-label="renderItem"
+          @select="selectItem" />
+        <!-- :on-update:value="getOptions"   -->
+      </n-card>
+      <div v-html="selection" style="text-align: left"></div>
     </div>
     <div v-else style="text-align: center">...loading</div>
   </div>
@@ -22,13 +24,32 @@ store.state.token = 'test';
 const query = ref('');
 const options = ref([]);
 
-const getOptions = async () => {
-  const res = await store.get('suggestions', query.value);
+const selection = ref('');
+
+const renderItem = (x: any) => `${x.label}  [${x.lid}]`;
+
+const selectItem = async (id: number) => {
+  // console.log(id);
+  const res = await store.get('item', id);
   // console.log(res);
-  options.value = res.map((item: any) => ({
-    label: `${item.title} (${item.lid})`,
-    value: item.id
-  }));
+  const body = res.body.replace(/\n/mg, "<br/>").replace(/\<c\s+c\=\"/mg, '<span style="color:').replace(/\<\/c\>/mg, '</span>');
+  selection.value = body;
+};
+
+const getOptions = async () => {
+  if (query.value) {
+    const res = await store.get('suggestions', query.value);
+    options.value = res.map((item: any) => ({
+      label: `${item.title}`,
+      value: item.id,
+      lid: item.lid
+    }));
+    if (res?.[0]) {
+      await selectItem(res.shift().id);
+    } else {
+      console.log('0 for', query.value);
+    }
+  }
 };
 
 
@@ -43,3 +64,16 @@ onBeforeMount(async () => {
   isLoaded.value = true;
 });
 </script>
+
+
+<style>
+.root {
+  color: silver;
+  font-weight: bold;
+}
+
+.spec {
+  font-style: italic;
+  /* font-style: oblique; */
+}
+</style>
