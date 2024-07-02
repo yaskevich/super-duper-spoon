db16a878b2b1f9262744adfd737a23c6c26dd892
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-07-02 23:06:10 +0200

    create component

---
+++ client/src/components/History.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="text-align: center; max-width: 350px; margin: auto">
+        <n-card :bordered="false">
+            {{ store?.state?.user?.queries }}
+        </n-card>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref, reactive, onBeforeMount, computed } from 'vue';
+import store from '../store';
+</script>
