7c7b0c036d10fe0ca12c52c6851bb10e290c6037
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-07-07 22:00:55 +0200

    render items as buttons

--- client/src/components/History.vue
+++ client/src/components/History.vue
@@ -1,11 +1,20 @@
 <template>
     <div style="text-align: center; max-width: 350px; margin: auto">
         <n-card :bordered="false">
-            {{ store?.state?.user?.queries }}
+            <n-space vertical v-if="store?.state?.user?.queries?.length">
+                <div v-for="item in store?.state?.user?.queries">
+                    <n-button @click="$router.push('/' + item)">
+                        {{ item }}
+                    </n-button>
+                </div>
+            </n-space>
+            <div v-else>
+                No items
+                {{ store?.state?.user?.queries }}
+            </div>
         </n-card>
     </div>
 </template>
-
 
 <script setup lang="ts">
 import { ref, reactive, onBeforeMount, computed } from 'vue';
